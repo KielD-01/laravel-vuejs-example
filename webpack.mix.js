e763210e0806c8403ff2dead21f5e44c0c983059
Roman Kozin <romanko95.rk@gmail.com>
2018-08-07 10:13:55 +0300

    Updated webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/admin/app.js', 'public/js/admin')
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.version();
+
+mix.browserSync('l.vue');
